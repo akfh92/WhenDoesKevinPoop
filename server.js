5f637dc7a6f322ecce1f5e24d5c629d9bc35d597
m <sanghyeok.park@pepperdine.edu>
2023-05-07 00:53:41 -0700

    started on checkedPoop()

--- server.js
+++ server.js
@@ -23,35 +23,45 @@ async function checkLastGame() {
     //get api from riot match history with count of 1 
     let url = 'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/Hu46PTPaSMgs3zU3HU4RyfVdQHkBKWHssLEaj8vWfd_19qzXg3xlKu_AsUkBQp1_EG-lSge7NXRx4A/ids?start=0&count=1&api_key=' + config.RIOT_API_KEY;
     let response = await fetch(url);
-    let data = await response.json()
+    let data = await response.json();
     //check this with global variable(LastGame) and if they match return false
     if(data[0]==LastGame){
-        return false;
+        return [false,''];
     }
     //else return true
     LastGame = data[0];
-    return true;
+    return [true,data];
 
 }
+//-------------------------checkPooped-------------------------//
+async function checkPooped(){
+    let url = 'https://americas.api.riotgames.com/lol/match/v5/matches/NA1_4648782588?api_key='+ config.RIOT_API_KEY;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+}
+//-------------------------checkPooped-------------------------//
+
+
+
+
 //-------------------------checkLastGame-------------------------//
-
-
 
 //————————————MAIN FUNCTION————————————//
 async function main(){
     setInterval(async function () {
         //call a function that returns true if kevin played another game                           -> function name checkLastGame()
-        // let jsondata = await checkLastGame();
-        console.log(await checkLastGame());
-        // console.log(jsondata);
+        //let lastGameCheck = await checkLastGame(); 
+
         // if checkLastGame returns true, check if he pooped; if pooped, return true.              -> function name checkPooped()
+        checkPooped();
     
         //if checkPooped return true, send kakao notification;                                     ->  function name sendKakako()
     
     
     
     
-    }, 2000)
+    }, 2000) // 120000 miliseconds = 20 minutes
 }
 //————————————MAIN FUNCTION————————————//
 main()
