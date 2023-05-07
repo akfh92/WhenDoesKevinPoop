8737f61e43db7a3172c29cec721556b335a181f8
m <sanghyeok.park@pepperdine.edu>
2023-05-07 00:16:09 -0700

    cleanup and added checkLastGame

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 // require
 const express = require("express");
 const fetch = require("node-fetch");
@@ -8,37 +7,51 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const http = require('http');
 const { config } = require(__dirname + "/config.js");
 
-let url = 'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/Hu46PTPaSMgs3zU3HU4RyfVdQHkBKWHssLEaj8vWfd_19qzXg3xlKu_AsUkBQp1_EG-lSge7NXRx4A/ids?start=0&count=20&api_key=' + config.RIOT_API_KEY;
-
+////GLOBAL VARIABLES////
+let LastGame = 'NA1_4648782588';
+//Main page
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 
 });
-app.get("/checkGame", function (req, res) {
-    // res.send(didKevinPoop())
-    console.log(didKevinPoop());
-});
 app.listen(3000, function () {
     console.log("linstening on 5000");
-    getLastTenGames();
 });
 
+//-------------------------checkLastGame-------------------------//
+async function checkLastGame() {
+    //get api from riot match history with count of 1 
+    let url = 'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/Hu46PTPaSMgs3zU3HU4RyfVdQHkBKWHssLEaj8vWfd_19qzXg3xlKu_AsUkBQp1_EG-lSge7NXRx4A/ids?start=0&count=1&api_key=' + config.RIOT_API_KEY;
+    let response = await fetch(url);
+    let data = await response.json()
+    //check this with global variable(LastGame) and if they match return false
+    if(data[0]==LastGame){
+        return false;
+    }
+    //else return true
+    LastGame = data[0];
+    return true;
 
-
-
-//// get last 10 games Kevin pooped ////
-
-// Stack of last 10 games Kevin pooped
-
-//function to get last 10 games Kevin Pooped
-async function getLastTenGames(lastGame) {
-    const response = await fetch(url);
-    let data = await response.json();
-    console.log(data);
 }
+//-------------------------checkLastGame-------------------------//
 
-//alert everytime kevin poop
-// async function kevinPoop() {    
 
-// }
 
+//————————————MAIN FUNCTION————————————//
+async function main(){
+    setInterval(async function () {
+        //call a function that returns true if kevin played another game                           -> function name checkLastGame()
+        // let jsondata = await checkLastGame();
+        console.log(await checkLastGame());
+        // console.log(jsondata);
+        // if checkLastGame returns true, check if he pooped; if pooped, return true.              -> function name checkPooped()
+    
+        //if checkPooped return true, send kakao notification;                                     ->  function name sendKakako()
+    
+    
+    
+    
+    }, 2000)
+}
+//————————————MAIN FUNCTION————————————//
+main()
