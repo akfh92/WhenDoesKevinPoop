2bd7f897204d927dc83eab26a4e3ae2ce4c5c6d4
m <sanghyeok.park@pepperdine.edu>
2023-05-07 21:13:46 -0700

    enabled email feature.

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // require
 const express = require("express");
 const fetch = require("node-fetch");
+const nodemailer = require('nodemailer');
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 const http = require('http');
+const { kakao_config } = require("./config");
 const { config } = require(__dirname + "/config.js");
 
 ////GLOBAL VARIABLES////
@@ -18,14 +20,14 @@ app.listen(3000, function () {
     console.log("linstening on 5000");
 });
 
-//-------------------------checkLastGame-------------------------//
+//————————————————————————checkLastGame————————————————————————//
 async function checkLastGame() {
     //get api from riot match history with count of 1 
     let url = 'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/Hu46PTPaSMgs3zU3HU4RyfVdQHkBKWHssLEaj8vWfd_19qzXg3xlKu_AsUkBQp1_EG-lSge7NXRx4A/ids?start=0&count=1&api_key=' + config.RIOT_API_KEY;
     let response = await fetch(url);
     let data = await response.json();
     //check this with global variable(LastGame) and if they match return false
-    if(data[0]==LastGame){
+    if (data[0] == LastGame) {
         return false;
     }
     //else return true
@@ -35,55 +37,108 @@ async function checkLastGame() {
 }
 
 
-//-------------------------checkPooped-------------------------//
-async function checkPooped(){
-    
-    let url = 'https://americas.api.riotgames.com/lol/match/v5/matches/'+LastGame+'?api_key='+ config.RIOT_API_KEY;
+//————————————————————————checkMatch————————————————————————//
+async function checkMatch() {
+
+    let url = 'https://americas.api.riotgames.com/lol/match/v5/matches/' + LastGame + '?api_key=' + config.RIOT_API_KEY;
     let response = await fetch(url);
     let data = await response.json();
     //find participant
-    jsonData1=data.metadata.participants;
+    jsonData1 = data.metadata.participants;
     let participantNum;
-    for(let i=0; i<jsonData1.length;i++){
-        if(jsonData1[i]==config.puuid){
-            participantNum=i;
-        }    
+    for (let i = 0; i < jsonData1.length; i++) {
+        if (jsonData1[i] == config.puuid) {
+            participantNum = i;
+        }
     }
     //console.log(data.info.participants[participantNum]);
-    let retObject={
-        kda:data.info.participants[participantNum].kda
+    let retObject = {
+        kda: data.info.participants[participantNum].challenges.kda,
+        kill: data.info.participants[participantNum].kills,
+        death: data.info.participants[participantNum].deaths,
+        assist: data.info.participants[participantNum].assists,
+        lane: data.info.participants[participantNum].lane,
+        win: data.info.participants[participantNum].win
     };
-    console.log(data.metadata.participants[participantNum]);
-    console.log(data.info.participants[participantNum].challenges.kda);
-    
+    return retObject;
 
-    
+
+
 }
-//-------------------------checkPooped-------------------------//
+//————————————————————————checkMatch-————————————————————————//
+
+
+
+//————————————————————————checkPooped————————————————————————//
+function checkPooped(inGameData) {
+
+    kill_death_diff = inGameData.death - inGameData.kills;
+    if (kill_death_diff >= 5) {
+        return true;
+    }
+    return false;
 
 
 
 
-//-------------------------checkLastGame-------------------------//
+}
+//————————————————————————checkPooped————————————————————————//
 
 
+//————————————————————————sendEmail————————————————————————//
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'whendoeskevinpoop@gmail.com',
+        pass: config.GMAIL_PASS
+    }
+});
 
-//————————————MAIN FUNCTION————————————//
-async function main(){
+
+function sendEmail(inGameData){
+    let stringMessage = JSON.stringify(inGameData);
+    var mailOptions = {
+        from: 'whendoeskevinpoop@gmail.com',
+        to: 'akfh92@live.com',
+        subject: 'KEVIN HAS POOPED!',
+        text: stringMessage
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+}
+
+//————————————————————————sendEmail————————————————————————//
+
+//————————————————————————MAIN FUNCTION————————————————————————//
+async function main() {
     setInterval(async function () {
         //call a function that returns true if kevin played another game                           -> function name checkLastGame()
         let lastGameCheck = await checkLastGame();
-        
 
-        // if checkLastGame returns true, check if he pooped; if pooped, return true.              -> function name checkPooped()
-        checkPooped();
-    
-        //if checkPooped return true, send kakao notification;                                     ->  function name sendKakako()
-    
-    
-    
-    
-    }, 200000) // 120000 miliseconds = 20 minutes
+
+        // if checkLastGame returns true, call check match which returns in-game data;             -> function name checkMatch()
+        if(lastGameCheck){
+            let inGameData = await checkMatch();
+        }
+        // if checkLastGame returns true, check if kevin pooped in the game;                       -> function name checkMatch()
+        if(lastGameCheck){
+            console.log(checkPooped(inGameData));
+        }
+
+
+        //if checkPooped return true, send kakao notification;                                     ->  function name sendEmail()
+        if(lastGameCheck){
+        sendEmail(inGameData);
+        }
+
+
+
+    }, 120000) // 120000 miliseconds = 20 minutes
 }
 //————————————MAIN FUNCTION————————————//
 main()
