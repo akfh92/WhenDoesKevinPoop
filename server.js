0fad6e3ded08a0a408312da6c6d1a95eb479ddff
James Park <sanghyeok.park@pepperdine.edu>
2023-05-15 20:48:05 -0700

    started on ejs

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 //const https = require("")
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 const https = require("https");
 const { config } = require(__dirname + "/config.js");
 // const PORT = config.PORT || 3030;
@@ -14,20 +14,19 @@ const PORT = 3030;
 
 //————————————————————————Global Variables————————————————————————//
 let LastGame = "NA1_4648782588";
+let getTime;
 //————————————————————————Global Variables————————————————————————//
 
-
 //————————————————————————loadMainPage————————————————————————//
-app.use(express.static(__dirname+"public"));
-app.use(express.static(__dirname+"styles"));
-app.get('/',function(req,res){
-  res.render("mainPage")
+app.use(express.static("public"));
+let currentLevel = await getSummonerInfo();
+app.get("/", function (req, res) {
+  res.render("mainPage", { data: { updatedTime: getTime, level:currentLevel} });
 });
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
 //————————————————————————loadMainPage————————————————————————//
-
 
 //————————————————————————checkLastGame————————————————————————//
 async function checkLastGame() {
@@ -46,7 +45,6 @@ async function checkLastGame() {
   return true;
 }
 //————————————————————————checkLastGame————————————————————————//
-
 
 //————————————————————————checkMatch————————————————————————//
 async function checkMatch() {
@@ -77,7 +75,6 @@ async function checkMatch() {
 }
 //————————————————————————checkMatch-————————————————————————//
 
-
 //————————————————————————checkPooped————————————————————————//
 function checkPooped(inGameData) {
   kill_death_diff = inGameData.death - inGameData.kill;
@@ -87,7 +84,6 @@ function checkPooped(inGameData) {
   return false;
 }
 //————————————————————————checkPooped————————————————————————//
-
 
 //————————————————————————sendEmail————————————————————————//
 var transporter = nodemailer.createTransport({
@@ -115,40 +111,66 @@ function sendEmail(inGameData) {
   });
 }
 
-//————————————————————————sendEmail————————————————————————//
+//————————————————————————sendEmail————————————————————————//ƒ
+
+
+//————————————————————————getCurrentTime————————————————————————//
+function getCurrentTime() {
+  var currentdate = new Date();
+  var datetime =
+    "Last Sync: " +
+    currentdate.getDate() +
+    "/" +
+    (currentdate.getMonth() + 1) +
+    "/" +
+    currentdate.getFullYear() +
+    " @ " +
+    currentdate.getHours() +
+    ":" +
+    currentdate.getMinutes() +
+    ":" +
+    currentdate.getSeconds();
+  return currentdate;
+}
+//————————————————————————getCurrentTime————————————————————————//
+
+
+//————————————————————————getSummonerInfo————————————————————————//
+//returns summoner level
+async function getSummonerInfo(){
+url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/Hu46PTPaSMgs3zU3HU4RyfVdQHkBKWHssLEaj8vWfd_19qzXg3xlKu_AsUkBQp1_EG-lSge7NXRx4A?api_key="+config.RIOT_API_KEY;
+let response = await fetch(url);
+let data = await response.json();
+console.log(data.summonerLevel);
+}
+//————————————————————————getSummonerInfo————————————————————————//
 
 
 //————————————————————————MAIN FUNCTION————————————————————————//
 async function main() {
   setInterval(async function () {
+    getTime = getCurrentTime();
     let lastGameCheck = false;
     //call a function that returns true if kevin played another game, returns false if kevin didn't play another game.                ----> function name checkLastGame()
     lastGameCheck = await checkLastGame();
-    console.log( "Kevin played another game: " + lastGameCheck);
+    console.log("Kevin played another game: " + lastGameCheck + " @:"+getTime);
 
     // if checkLastGame returns true, call check match which returns in-game data;                                                    -----> function name checkMatch()
     if (lastGameCheck) {
       let inGameData = await checkMatch();
-      inGameData2=inGameData;
+      inGameData2 = inGameData;
     }
 
     // if checkLastGame returns true, check if kevin pooped in the game;                                                              -----> function name checkPooped()
     if (lastGameCheck) {
-      checkPoopedOutput=checkPooped(inGameData2);
+      checkPoopedOutput = checkPooped(inGameData2);
     }
 
     //if checkPooped return true, send email notification;                                                                             ----->  function name sendEmail()
-    if (checkPoopedOutput) {
+    if (lastGameCheck&&checkPoopedOutput) {
       sendEmail(inGameData2);
     }
-
-     
   }, 1200); // 120000 miliseconds = 20 minutes
 }
 main(); // call main function
 //————————————MAIN FUNCTION————————————//
-
-
-
-
-
